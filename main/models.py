e38818c9cf0c64e4f7a357d8887f7c30c61643b8
Vinicius <vwnx123@gmail.com>
2022-12-14 09:35:47 -0300

    .

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
+
+
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    category_slug = models.SlugField(null=True)
+    category_slug = models.SlugField(null=True, blank=True)
 
 
     def __str__(self):
@@ -12,14 +15,15 @@ class Category(models.Model):
 
 
     def save(self, *args, **kwargs):  # new
-        if not self.slug:
-            self.slug = slugify(self.title)
+        if not self.category_slug:
+            self.category_slug = slugify(self.name)
         return super().save(*args, **kwargs)
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     post_category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    post_content = models.TextField()
+    post_content = RichTextField(blank=True, null=True)
+    # post_content = models.TextField()
     date_added = models.DateField(auto_now_add=True)
     post_slug = models.SlugField()
     post_img = models.ImageField(null=True, blank=True, upload_to="images/")
